01a8b07839bfb73e1574cf823d04949899c3c72f
metndurn <mtndrn123@gmail.com>
2025-06-15 03:13:58 +0300

    Butun seri videolari bitti egitim son buldu

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const navToggle = document.querySelector(".nav_toggle"),
 
 
     navToggle.addEventListener("click", () => {
-        navMenu.classList.toggle("show-menu");
+        navMenu.classList.toggle("show_menu");
         navToggle.classList.toggle("active");
     })
 
@@ -16,3 +16,46 @@ const navToggle = document.querySelector(".nav_toggle"),
         backSpeed: 70,
         loop: true,
     })
+
+
+
+// SWİPER hazır kutuphane kodlarıdır
+
+var swiper = new Swiper(".brand_container", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+
+      loop: true,
+  });   
+
+
+
+  // BACK TOP BUTTON 
+
+  const back_btn_top = document.querySelector(".btn_top");
+
+  window.addEventListener("scroll", () => {
+    if(window.scrollY >= 50){
+       back_btn_top.classList.add("active")
+    }else{
+        back_btn_top.classList.remove("active")
+    }
+  })
